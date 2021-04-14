f6b1f60ca1fb2da4bdc4de74cf7ad7dafdd61f48
Jimme <hendrixjimme@gmail.com>
2021-04-14 11:03:11 +0200

    Update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,9 @@ module.exports = {
     env: {
         browser: true,
         node: true,
-        es2021: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/recommended',
-        'prettier',
-        'prettier/vue',
-    ],
+    extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier'],
+    plugins: ['prettier'],
     rules: {
         'vue/no-unused-vars': 'error',
         'prettier/prettier': ['error'],
